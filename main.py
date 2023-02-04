1427902573301c3b409dcf32ca61eb122003d960
Pedro Garcia <pedro.metalurgia@gmail.com>
2023-02-04 18:58:54 -0300

    run

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.secret_key = 'agilim'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1512'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'pythonlogin'
 
 # Intialize MySQL
