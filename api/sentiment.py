b3b9d3a577da332845567bccb829ec3ec769b957
Pedro Saint Clair Garcia <pedro.garcia@take.net>
2023-02-23 21:04:18 -0300

    sentimentMultinomialNBmodel

--- api/sentiment.py
+++ api/sentiment.py
@@ -1,4 +1,7 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 class sentiment:
   def __init__(self, message=''):
@@ -21,9 +24,25 @@ class sentiment:
     scores = sid.polarity_scores(self.text)
     return scores
 
+  def sentimentMultinomialNBModel(self):
+    dataset = pd.read_csv('Tweets_Mg.csv',encoding='utf-8')
+    tweets = dataset["Text"].values
+    classes = dataset["Classificacao"].values
+    vectorizer = CountVectorizer(analyzer = "word")
+    freq_tweets = vectorizer.fit_transform(tweets)
+    modelo = MultinomialNB()
+    modelo.fit(freq_tweets, classes)    
+    example = [self.text]
+    freq_teste = vectorizer.transform(example)
+    return modelo.predict(freq_teste)[0]
+
   def __del__(self):
         pass #print('Destructor called')
 
 if __name__ == "__main__":
     data = sentiment("I love you!")
     print(data.englishSentiment())
+    data.set_text("Esse governo está no início, vamos ver o que vai dar")
+    print(data.sentimentMultinomialNBModel())
+
+    
