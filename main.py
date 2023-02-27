783c387fee899659383470324fde9df949f0679c
Pedro Saint Clair Garcia <pedro.garcia@take.net>
2023-02-27 20:00:14 -0300

    production deploy

--- main.py
+++ main.py
@@ -134,5 +134,11 @@ def back_home():
     
     return render_template('home.html')
 
+def production():
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=5000)
+
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
+    #production()
